41c152077a15c2c4b1a2a162ad6c6f4becf5f822
caonabo castro <plattaneroz@gmail.com>
2023-02-27 00:21:48 -0500

    singInwithEamilAndPassword is working

--- src/Component/SignUP/SignUp.js
+++ src/Component/SignUP/SignUp.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import {Grid} from '@mui/material'; // Grid version 1
 import './SignUp.css';
 import plata from "../../Images/plata.png";
@@ -6,107 +7,63 @@ import p from "../../Images/pp.png"
 import { display, height } from '@mui/system';
 import { UserAuth } from '../../Context/AuthContext'; 
 import Button from '@mui/material/Button';
-import { Link } from 'react-router-dom';
+
 //import './App.css';
 
-class SignUp extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            email:null,
-            fullName:null,
-            userName:null,
-            password:null,
-            error:''
-        }
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    
+const Signup = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('')
+    const { createUser } = UserAuth();
+    const navigate = useNavigate()
   
-     handleChange(event){
-        
-        this.setState({emailOrNumber: event.target.emailOrNumber});
-        this.setState({fullName: event.target.fullName});
-        this.setState({userName: event.target.userName});
-        this.setState({password: event.target.password});
-    }
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      setError('');
+      try {
+        await createUser(email, password);
+        navigate('/account')
+      } catch (e) {
+        setError(e.message);
+        console.log(e.message);
+      }
+    };
+  
+    return (
+      <div className='max-w-[700px] mx-auto my-16 p-4'>
+        <div>
+          <h1 className='text-2xl font-bold py-2'>Sign up for a free account</h1>
+          <p className='py-2'>
+            Already have an account yet?{' '}
+            <Link to='/' className='underline'>
+              Sign in.
+            </Link>
+          </p>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <div className='flex flex-col py-2'>
+            <label className='py-2 font-medium'>Email Address</label>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              className='border p-3'
+              type='email'
+            />
+          </div>
+          <div className='flex flex-col py-2'>
+            <label className='py-2 font-medium'>Password</label>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              className='border p-3'
+              type='password'
+            />
+          </div>
+          <button className='border border-blue-500 bg-blue-600 hover:bg-blue-500 w-full p-4 my-2 text-white'>
+            Sign Up
+          </button>
+        </form>
+      </div>
+    );
+  };
+  
+  export default Signup;
 
-    handleSubmit = async (event) =>{
-        const { createUser } = UserAuth();
-        event.preventDefault();
-        
-        this.state.error= ''
-        try{
-            await createUser(this.state.email, this.state.password);
-        } catch(e){
-            this.state.error= e.message;
-            console.log(this.state.error);
-        }
-        alert('A name was submitted: ' + this.state);
-
-    }
-
-    render(){
-        return(
-            <div>
-                
-                <Grid container >
-                        <Grid  item xs={5}>
-                            
-                        </Grid>
-                        <Grid className="MidGrid"  item xs={2}>
-                           
-                            <form className="singUpcontainer">
-                                
-                                            
-                                <div  className="singUpInnerContianer"  >
-                                        <img  className='PlataSign' src={plata} style={{height:80 }}/>
-            
-
-                                        <h2 className='signUpText'>Sign up to see photos and videos from your friends.</h2>
-                                    
-                                        <button className="loginWithFaceBookButton" >
-                                        <span className='Logo'>P</span> Login with FaceBook
-                                        </button>
-                                        <div className='OrContainer'>
-                                        <div className='OrLines'></div><div className='OrLetters'>OR</div><div className='OrLines'></div>
-                                        </div>
-                                        <div className='InputFields'>
-                                            <input className='InputBox' name="emailOrNumber" required="true" type = "text" placeholder='Mobile Number or Email'
-                                            onChange={(event)=>{this.state.emailOrNumber=event.currentTarget.value;}}
-                                            />
-                                            <input className='InputBox' name="fullName" required="true" type="text" placeholder='Full Name'
-                                                onChange={(event)=>{this.state.fullName=event.currentTarget.value;}}
-                                            />
-                                            <input className='InputBox' name ="userName" required="true" type="text" placeholder='Username'
-                                                onChange={(event)=>{this.state.userName=event.currentTarget.value;}}
-                                            />
-                                            <input className='InputBox' name="password" required={true} type ="password" aria-required="true"
-                                                autoCapitalize="off" autoComplete="new-password" autoCorrect="off" placeholder='Password'
-                                                onChange={(event)=>{this.state.password=event.currentTarget.value;}}   
-                                                    />
-                                        </div>
-                                        <button className="SignUPButton" value="submit" onClick={this.handleSubmit} >
-                                        Sign up  
-                                        </button>
-                                </div>
-
-                                <div className='LoginContainer'>
-                                   <p className='LoginText'> Have an account? <Link to='/'>Login</Link> </p>
-                                </div>
-                            </form>
-                             
-                        </Grid>
-                        <Grid   item xs={5}>
-                            
-                        </Grid>
-                    
-                </Grid>
-            </div>
-        );
-    }
-}
-
-export default SignUp;
